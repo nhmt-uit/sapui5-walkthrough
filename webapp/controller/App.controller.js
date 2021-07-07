dc79acece5ad14d99ad4b98585dafaded20b9648
nhmt-uit <096666671x@gmail.com>
2021-07-07 13:28:09 +0700

    step 6 - Modules

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,12 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
-], function (Controller) {
+    'sap/m/MessageToast'
+], function (Controller, MessageToast) {
     'use strict';
 
     return Controller.extend("sap.ui.demo.walkthrough.App", {
         onShowHello: function(){
-            alert("You clicked Say Hi and I say hi to you!")
+            MessageToast.show("Hello there!! ")
         }
     });
 });
