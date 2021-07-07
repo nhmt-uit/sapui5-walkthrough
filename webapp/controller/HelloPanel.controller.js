998ae005ef576b6ea416ced12f9a2f3a8f899dc9
nhmt-uit <096666671x@gmail.com>
2021-07-07 15:49:20 +0700

    Update Step 15

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -2,7 +2,7 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/core/Fragment"
-], function (Controller, MessageToas, Fragmentt) {
+], function (Controller, MessageToast, Fragment) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
         onShowHello: function () {
