0b7df644bdbe7d49b43598d459c294caa3e62364
nhmt-uit <096666671x@gmail.com>
2021-07-08 09:21:55 +0700

    Step 31: Routing and Navigation

--- webapp/component.js
+++ webapp/component.js
@@ -1,42 +1,35 @@
 sap.ui.define([
-    'sap/ui/core/UIComponent',
-    'sap/ui/model/json/JSONModel',
-    'sap/ui/model/resource/ResourceModel',
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel",
 	"./controller/HelloDialog"
-], function(UIComponent, JSONModel, ResourceModel, HelloDialog) {
-    'use strict';
-    return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
-        metadata: {
-            rootView: {
-                viewName: "sap.ui.demo.walkthrough.view.App",
-                type: 'XML',
-                async: true,
-                id: 'app'
-            }
-        },
+], function (UIComponent, JSONModel, HelloDialog) {
+	"use strict";
 
-        init : function () {
-            // call the init function of the parent
-            UIComponent.prototype.init.apply(this, arguments);
-            // set data model
-            var oData = {
-               recipient : {
-                  name : "World"
-               }
-            };
-            var oModel = new JSONModel(oData);
-            this.setModel(oModel)
-   
-            // set i18n model
-            var i18nModel = new ResourceModel({
-               bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
-            });
-            this.setModel(i18nModel, "i18n");
-        // set dialog
-			this._helloDialog = new HelloDialog(this.getRootControl());
+	return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
+		metadata: {
+			manifest: "json"
 		},
 
-		exit : function() {
+		init: function () {
+			// call the init function of the parent
+			UIComponent.prototype.init.apply(this, arguments);
+
+			// set data model
+			var oData = {
+				recipient: {
+					name: "UI5"
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.setModel(oModel);
+
+			// set dialog
+			this._helloDialog = new HelloDialog(this.getRootControl());
+			// create the views based on the url/hash
+			this.getRouter().initialize();
+		},
+
+		exit : function () {
 			this._helloDialog.destroy();
 			delete this._helloDialog;
 		},
@@ -44,5 +37,6 @@ sap.ui.define([
 		openHelloDialog : function () {
 			this._helloDialog.open();
 		}
-    })
+	});
+
 });
